394f9d38f1aad4bd5362cefc2d1757ccf432ae55
tuando18 <tuaansdo.18@gmail.com>
2023-09-12 19:52:51 +0700

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+// day la cmt de giai thich code
+console.log("Đây là dòng lệnh console log");
+
+console.error("Đây là dòng lệnh console error");
+
+console.debug("Đây là dòng lệnh console debug");
+// Demo sử dụng lệnh alert
+// alert("Đây là dòng lệnh alert");
+
+// Demo sử dụng prompt
+// Tham số 1: Tên của Input
+// Tham số 2: Dữ liệu mặc định
+// prompt("Nhập vào dữ liệu hiện tại:" , "Đây là dữ liệu mặc định");
+
+// Demo console ra giá trị nhập vào từ prompt
+// console.log(prompt("Nhập vào tên trường: "));
+
+// Demo lệnh math.random
+// trả về số random từ 0 đến 1
+console.log("Đây là số random: " + Math.random() * 10);
+
+// Ví dụ thêm về console log
+
+console.log("Đây là tên trường vừa nhập: " + prompt("Nhập vào tên trường: "));
